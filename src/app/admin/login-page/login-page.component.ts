0ee5a65b5d297f7bc5a6e9e3ec362c959ee4e6a6
Grigorii Balabanov <810411@mail.ru>
2020-04-03 17:03:29 +0500

    firebase, login, route guard

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
+import {AuthService} from '../shared/services/auth.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -10,30 +12,54 @@ import {User} from '../../shared/interfaces';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submitted = false;
+  message: string;
 
-  constructor() { }
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+  }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params.loginAgain) {
+        this.message = 'Войдите заново';
+      }
+    });
+
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
         Validators.email
       ]),
-      password: new FormControl( null, [
+      password: new FormControl(null, [
         Validators.required,
         Validators.minLength(6)
       ])
     });
   }
+
   submit() {
     if (this.form.invalid) {
       return;
     }
+
+    this.submitted = true;
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
     };
-    console.log(user);
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['admin', 'dashboard']);
+      this.submitted = false;
+    }, () => {
+      this.submitted = false;
+    });
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+import keys from '../../keys';
+
+export const environment: Environment = {
+  production: true,
+  apiKey:  keys.apiKey
 };
